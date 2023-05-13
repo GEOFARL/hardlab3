0c83750f82b935216124aec21777f7b9db0d482e
Maxim <toper.one111@gmail.com>
2023-05-13 09:32:20 +0300

    Create a structure to represent an operator

--- src/RPNConverter.h
+++ src/RPNConverter.h
@@ -2,6 +2,16 @@
 #include <string>
 #include "Stack.h"
 
+struct Operator
+{
+  std::string symbol;
+  int precedence;
+  bool isLeftAssociative;
+
+  Operator(const std::string &sym, int prec, bool isLeftAssoc)
+      : symbol(sym), precedence{prec}, isLeftAssociative{isLeftAssoc} {}
+};
+
 template <typename T>
 class RPNConverter
 {
